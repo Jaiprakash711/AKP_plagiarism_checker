b2e82fbba9b1d1afe81bbf6c447275997194338a
Jaiprakasb21 <jaiprakash9684@gmail.com>
2025-02-21 19:44:49 +0530

    Update V000001__INITIAL_STRUCTURE.sql

--- src/main/resources/db/migration/V000001__INITIAL_STRUCTURE.sql
+++ src/main/resources/db/migration/V000001__INITIAL_STRUCTURE.sql
@@ -1,15 +1,7 @@
-CREATE SEQUENCE plagiarism_check_seq START 1;
 CREATE SEQUENCE upload_text_seq START 1;
-
-CREATE TABLE upload_text_entity (
-    id BIGINT PRIMARY KEY DEFAULT nextval('upload_text_seq'),
-    text_content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
 CREATE TABLE plagiarism_check (
     id BIGINT PRIMARY KEY DEFAULT nextval('plagiarism_check_seq'),
-    upload_text_id BIGINT NOT NULL,
     service VARCHAR(255) NOT NULL,
     scan_time VARCHAR(255) NOT NULL,
     input_type VARCHAR(50) NOT NULL,
@@ -20,5 +12,4 @@ CREATE TABLE plagiarism_check (
     identical_word_counts INT NOT NULL,
     similar_word_counts INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (upload_text_id) REFERENCES upload_text_entity(id) ON DELETE CASCADE
 );
